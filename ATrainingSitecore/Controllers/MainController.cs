9d661df9800a506ad12efb33293173288897b735
Иван Кондрашов <vanektroll@gmail.com>
2016-05-25 10:09:10 +0300

    2nd commit

---
+++ ATrainingSitecore/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ATrainingSitecore.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Default
+        public ActionResult DigitalAsset()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Countries()
+        {
+            return View();
+        }
+
+        public ActionResult Header()
+        {
+            var home_item = Sitecore.Context.Database.GetItem("{B4B5438B-08DF-4E60-9BBF-45F5F1508750}");
+            var menu_items = home_item.GetChildren().ToArray();
+            return View(menu_items);
+        }
+    }
+}
